70062faf2467ec60b727189428400432f5448702
Omar Mohamed <omar.kungfu12@gmail.com>
2023-07-14 16:11:03 +0300

    Form validation done !

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -5,8 +5,31 @@ export default function FormValidator () {
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
 
+  const [message, setMessage] = useState('')
+
+  const findErrors = () => {
+    const errors = []
+
+    if (!email || !password || !passwordConfirm) errors.push('All fields are required')
+    if ([...email].filter(char => char === '@').length !== 1) 
+    errors.push('Email must contain a @')
+    if (password.length < 8) errors.push('Password must be at least 8 characters')
+    if (passwordConfirm === '') errors.push('Password confirmation is required')
+    if (password !== passwordConfirm) errors.push('Passwords must match')
+
+    return errors
+  }
+  // preventDefault() is used to prevent the default action of the form
+  // from being triggered. In this case, the default action is to refresh
+  // the page. We don't want that to happen, so we use preventDefault().
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const errors = findErrors()
+    setMessage(errors.length ? errors.join(', ') : 'User created!')
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -23,6 +46,7 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
+      <p>{message}</p>
       <input type='submit' value='Submit' />
     </form>
   )
